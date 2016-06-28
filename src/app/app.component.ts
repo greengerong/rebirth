4bcf5a63cfcdd1b7ad266b587e9242698c634cdf
polang <grzx2210@163.com>
2016-06-28 16:17:07 +0800

    tslint codelyzer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,7 @@ import config from 'config';
 export class AppComponent {
 
   constructor(rebirthHttpProvider: RebirthHttpProvider) {
+
     rebirthHttpProvider
       .baseUrl(config.api.host)
       .json()
