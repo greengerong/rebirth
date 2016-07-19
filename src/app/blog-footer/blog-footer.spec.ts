9a2b296bbf1a0bf7e3904eae6fa75cfd044e8fce
xiaomo <83387856@qq.com>
2016-07-19 17:10:15 +0800

    blog footer test file

---
+++ src/app/blog-footer/blog-footer.spec.ts
@@ -0,0 +1,29 @@
+import { TestComponentBuilder } from '@angular/compiler/testing';
+import { addProviders, inject, async } from '@angular/core/testing';
+import { ElementRef } from '@angular/core';
+
+import { GLOBAL_PROVIDERS } from '../../global.providers';
+import { BlogFooterComponent } from './index';
+
+describe('Article aside Component', () => {
+
+  beforeEach(() => addProviders([
+    ...GLOBAL_PROVIDERS,
+    {
+      provide: ElementRef,
+      useValue: new ElementRef(document.body)
+    },
+    BlogFooterComponent,
+  ]));
+
+  it('should contain a number', async(inject([TestComponentBuilder],
+    (tcb: TestComponentBuilder) => {
+      tcb.createAsync(BlogFooterComponent)
+        .then((fixture) => {
+          fixture.detectChanges();
+          let elm: HTMLElement = fixture.nativeElement;
+          let linkElms = elm.querySelectorAll(".center-block");
+          expect(linkElms[0].textContent).toContain('2016');
+        });
+    })));
+});
