ab77ec9318614ff66258ad5ddca43ae2afeb0f8e
polang <grzx2210@163.com>
2016-07-27 21:00:12 +0800

    search component

--- src/app/manage-app/article-list/article-list.component.ts
+++ src/app/manage-app/article-list/article-list.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from "@angular/core";
 import { ArticleItemComponent } from "../article-item";
 import { ArticleService, Article, SearchResult, PagerComponent } from "common";
 import config from 'config';
+import { ArticleSearchComponent } from '../article-search';
 
 @Component({
   selector: 'article-list',
   template: require('./article-list.html'),
   styles: [require('./article-list.scss')],
-  directives: [ArticleItemComponent, PagerComponent],
+  directives: [ArticleItemComponent, PagerComponent, ArticleSearchComponent],
   providers: [ArticleService]
 })
 export class ArticleListComponent implements OnInit {
@@ -21,12 +22,15 @@ export class ArticleListComponent implements OnInit {
     this.queryArticles(1);
   }
 
+  searchArticle(keyword): void {
+    console.log(keyword, "============");
+  }
+
   private queryArticles(pageIndex: number) {
     this.articleService.getArticles(pageIndex, config.article.pageSize)
       .subscribe((result) => {
         this.article = result;
       });
   }
-
 
 }
--- src/app/manage-app/article-search/article-search.component.ts
+++ src/app/manage-app/article-search/article-search.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
-import { Article } from "../article-service/Article";
+import { Component, Input, Output, ChangeDetectionStrategy, EventEmitter } from '@angular/core';
+import { Article } from "common";
+import keyword = ts.ScriptElementKind.keyword;
 
 @Component({
   selector: 'article-search',
@@ -11,29 +12,10 @@ import { Article } from "../article-service/Article";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ArticleSearchComponent {
-
   @Input() articles: Article [];
+  @Output() searchArticle = new EventEmitter();
 
-
-  /**
-   * 筛选出符合要求的数据
-   * @param keyword
-   * @returns {Article[]}
-   */
-  filterArticles(keyword: string): Array<Article> {
-    return this.articles.filter((article: Article) => {
-      if (article.title.indexOf(keyword)) {
-        console.log(`title  关键词是${keyword}, 文章标题是 ${article.title}`);
-        return true;
-      }
-      let categories: string[] = article.categories;
-      for (let category of categories) {
-        console.log(category.indexOf(keyword));
-        if (category.indexOf(keyword)) {
-          console.log(`category  关键词是${keyword}, 分类是 ${category}`);
-          return true;
-        }
-      }
-    });
+  search(keyword): void {
+    this.searchArticle.emit(keyword);
   }
 }
