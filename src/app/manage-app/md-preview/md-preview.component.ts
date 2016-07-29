7c47c17e475e011476cb25d52fd402388160cdde
polang <grzx2210@163.com>
2016-07-29 21:10:57 +0800

    markdown editor

--- src/app/manage-app/md-preview/md-preview.component.ts
+++ src/app/manage-app/md-preview/md-preview.component.ts
@@ -8,7 +8,7 @@ import * as hljs from 'highlight.js';
   styles: [require('./md-preview.scss')],
   template: require('./md-preview.html')
 })
-export class MdPreviewComponent {
+export class MdPreviewComponent implements OnChanges {
   @Input('mdArticle') mdArticle: string;
   private mdHtmlText: string;
   private markdownIt: any;
@@ -17,11 +17,11 @@ export class MdPreviewComponent {
     this.markdownIt = (<any>markdownit)({
       html: true,
       highlight: function (code, lang) {
-        console.log(code, '===', lang, '===', hljs);
         if (lang && hljs.getLanguage(lang)) {
           try {
             return hljs.highlight(lang, code).value;
           } catch (e) {
+            return code;
           }
         }
         return code;
@@ -30,7 +30,6 @@ export class MdPreviewComponent {
   }
 
   ngOnChanges(event): void {
-    console.log(event, '=========');
     if (event.mdArticle) {
       this.mdHtmlText = event.mdArticle.currentValue ? this.markdownIt.render(event.mdArticle.currentValue) : '';
     }
