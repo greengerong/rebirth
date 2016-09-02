2330d90b1c54da709e495eb14e8976783b2f4460
polang <grzx2210@163.com>
2016-09-02 22:02:51 +0800

    update npm package

--- build/spec-bundle.js
+++ build/spec-bundle.js
@@ -11,18 +11,17 @@ Error.stackTraceLimit = Infinity;
 
 require('core-js/es6');
 require('core-js/es7/reflect');
-require('reflect-metadata');
 
 // Typescript emit helpers polyfill
 require('ts-helpers');
 
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
+require('zone.js/dist/proxy'); // since zone.js 0.6.15
+require('zone.js/dist/sync-test');
+require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 require('zone.js/dist/async-test');
 require('zone.js/dist/fake-async-test');
-require('zone.js/dist/sync-test');
-require('zone.js/dist/proxy'); // since zone.js 0.6.14
-require('zone.js/dist/jasmine-patch'); // put here since zone.js 0.6.14
 
 // RxJS
 require('rxjs/Rx');
