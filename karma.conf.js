50104f971834b3026743dea067bce221b9d6fc82
polang <grzx2210@163.com>
2017-10-21 16:02:12 +0800

    fix all ng5 test

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'], // Chrome
+    browsers: ['Chrome'], // Chrome
     singleRun: false
   });
 };
